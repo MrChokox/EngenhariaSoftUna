45271767f0f5e9d3a5614d88d6097a7c150b9490
Gualberto Junio <80179954+MrChokox@users.noreply.github.com>
2022-04-01 19:27:14 -0300

    Update Choconinho.java

--- FabricaDeBoloFactoryMethod/src/fabricadebolo/Choconinho.java
+++ FabricaDeBoloFactoryMethod/src/fabricadebolo/Choconinho.java
@@ -5,7 +5,7 @@ public class Choconinho implements AbstractBoloInterface{
 
     @Override
     public void sobreoBolo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("5 colheres de cacau, 2 copos de leite ninho, 7 ovos, 5 xicaras de farinha");
     }
 
     @Override
