72cd9de9d23b0eb947bc8164e7e36edc1fbeb004
Gualberto Junio Moreira Silva - 42113921 <42113921@ulife.com.br>
2022-05-13 20:38:42 -0300

    Açai questão 6

---
+++ AçaiComTamanhos/src/açaicomtamanhos/Adicional.java
@@ -0,0 +1,7 @@
+package açaicomtamanhos;
+
+public interface Adicional {
+    public abstract String getDescricao();
+    public abstract double custo();
+    public abstract void setTamanho(char t);
+}
